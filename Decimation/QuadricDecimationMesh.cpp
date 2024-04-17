9bb37f86e331aed2bd1f3945e356e8f007152f5a
Luckelina <93657078+Luckelina@users.noreply.github.com>
2024-04-17 14:32:53 +0200

    fixat skiten
    jej

--- Decimation/QuadricDecimationMesh.cpp
+++ Decimation/QuadricDecimationMesh.cpp
@@ -40,50 +40,52 @@ void QuadricDecimationMesh::computeCollapse(EdgeCollapse* collapse) {
 
     HalfEdge e1 = e(collapse->halfEdge);
     auto v1 = e1.vert;
-    auto v2 = e(e1.prev).vert;
+    auto v2 = e(e1.pair).vert;
 
     // Q is the sum of the quadrics at the edge endpoints
     glm::mat4 Q = mQuadrics[v1] + mQuadrics[v2];
+    
 
-    // Set the last row to (0, 0, 0, 1) for the minimization problem
-    Q[0][3] = 0;
-    Q[1][3] = 0;
-    Q[2][3] = 0;
-    Q[3][3] = 1;
+    glm::mat4 Qbar = Q;
+
+    //glm::mat4 Q1 = createQuadricForVert(v1);
+    //glm::mat4 Q2 = createQuadricForVert(v2);
+
+    //glm::mat4 Q = Q1 + Q2;
+
+
+    // Set the last row to (0, 0, 0, 1) for the minimization problem (this is Q-bar)
+    Qbar[0][3] = 0.0f;
+    Qbar[1][3] = 0.0f;
+    Qbar[2][3] = 0.0f;
+    Qbar[3][3] = 1.0f;
     glm::vec4 v_bar;
 
    
     //Check if invertable det(Q) != 0
-    if (abs(glm::determinant(Q)) > 0.0000000001f) {
+    if (abs(glm::determinant(Qbar)) > 0.0000000001f) {
     
-
-        v_bar = glm::inverse(Q) * glm::vec4(0, 0, 0, 1);
-    
-    
+        v_bar = glm::inverse(Qbar) * glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
     
     } else {
-    
-        Q = mQuadrics[v1] + mQuadrics[v2];
         
-        glm::vec3 v1Pos = v(v1).pos;
-        glm::vec3 v2Pos = v(v2).pos;
-        glm::vec3 midPos = (v1Pos + v2Pos) * 0.5f;
+        glm::vec4 v1Pos(v(v1).pos, 1.0f);
+        glm::vec4 v2Pos(v(v2).pos, 1.0f);
+        glm::vec4 midPos((v1Pos + v2Pos) * 0.5f);
        
-        
-        
-        glm::vec3 candidates[3] = {v1Pos, v2Pos, midPos};
+        std::vector<glm::vec4> candidates{v1Pos, v2Pos, midPos};
         float minError = std::numeric_limits<float>::max();
         for (const auto& candidate : candidates) {
-            glm::vec4 v_candidate = glm::vec4(candidate, 1.0f);
-            float error = glm::dot(v_candidate, Q * v_candidate);
+            //glm::vec4 v_candidate = candidate;
+            float error = glm::dot(candidate, Q * candidate);
             if (error < minError) {
                 minError = error;
-                v_bar = v_candidate;
+                v_bar = candidate;
             }
         }
     }
 
-    collapse->position = {v_bar.x,v_bar.y,v_bar.z};  // Set the target position for this collapse
+    collapse->position = glm::vec3(v_bar.x,v_bar.y,v_bar.z);  // Set the target position for this collapse
     collapse->cost = glm::dot(v_bar, Q * v_bar);
 
    // std::cerr << "computeCollapse in QuadricDecimationMesh not implemented.\n";
@@ -126,15 +128,11 @@ glm::mat4 QuadricDecimationMesh::createQuadricForFace(size_t indx) const {
     HalfEdge e1 = e(f(indx).edge);
     // Vertex positions of the face
     glm::vec3 v1 = v(e1.vert).pos;
-    glm::vec3 v2 = v(e(e1.next).vert).pos;
-    glm::vec3 v3 = v(e(e1.prev).vert).pos;
-
-    glm::vec3 normal2 = glm::normalize(glm::cross(v2 - v1, v3 - v1));
-
+   
     glm::vec3 normal1 = f(indx).normal;
-    float d = -glm::dot(normal2, v1); // ax + by + cz + d. This is the last component d.
+    float d = -glm::dot(normal1, v1); // ax + by + cz + d. This is the last component d.
 
-    glm::vec4 plane(normal2, d);                           // Extended plane coefficients to 4D. 
+    glm::vec4 plane(normal1, d);                           // Extended plane coefficients to 4D. 
     glm::mat4 quadric = glm::outerProduct(plane, plane);  // Compute quadric.
     //std::cout << "normal1:\n" << normal1.x << ", " << normal1.y << ", " << normal1.z << "\nnormal 2:\n" << normal2.x << ", " << normal2.y << ", " << normal2.z << std::endl;
 
